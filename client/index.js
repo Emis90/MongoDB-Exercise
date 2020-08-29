49406557b7c423b45efe3b4a14c7e7de3f5ac3d3
enida vezelaj <enidavezelaj@enidas-MBP.fios-router.home>
2020-08-29 13:15:43 -0400

    cleaning up code i dont need and added README

--- client/index.js
+++ client/index.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './components/Main'
-import { ApolloProvider } from "react-apollo";
-import { InMemoryCache, ApolloClient } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache(),
-});
-
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Main />
-  </ApolloProvider>,
+    <Main />,
   document.getElementById('app') 
 )
